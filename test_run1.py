edae45ce1e599b857a6a3580540b2f58611124a0
John <jdin0015@student.monash.edu>
2023-04-28 17:38:14 +1000

    Part 1 push for data run 2

--- test_run1.py
+++ test_run1.py
@@ -4,8 +4,8 @@ import os
 import matplotlib.pyplot as plt
 
 
-colour_image = o3d.io.read_image("images\image000003_left.png")
-depth_image = o3d.io.read_image("depth\image000003_disp_raw.png")
+colour_image = o3d.io.read_image("Run 2\images\image000000_left.png")
+depth_image = o3d.io.read_image("Run 2\depth\image000000_disp_raw.png")
 print(np.asarray(depth_image))
 rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(colour_image, depth_image, convert_rgb_to_intensity=False)
 print(rgbd_image)
@@ -16,5 +16,12 @@ plt.subplot(1, 2, 2)
 plt.imshow(rgbd_image.depth)
 plt.show()
 
-pcd=o3d.io.read_point_cloud("pcloud\image000003_3d.ply")
-o3d.visualization.draw_geometries([rgbd_image, pcd])
+pcd=o3d.io.read_point_cloud("Run 2\pcloud\image000000_3d.ply")
+#o3d.visualization.draw_geometries([rgbd_image])
+
+cam_prop = o3d.camera.PinholeCameraIntrinsic()
+cam_prop.height = 768
+cam_prop.width = 1024
+cam_prop.intrinsic_matrix = [[872.76442, 0, -766.876], [0, 872.76442, -574.1995], [0, 0, 1]]
+pcd_comp = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, cam_prop)
+o3d.visualization.draw_geometries([pcd_comp])
--- view_pt_cloud.m
+++ view_pt_cloud.m
@@ -1,13 +1,19 @@
 clear variables; close all; clc; 
 
 %% Read all input files and initialise the reprojection matrix
-disparityRaw = imread("depth\image000001_disp_raw.png"); 
-frameLeftRect = imread("images\image000001_left.png");
-%frameRightRect = imread("images\image00000#_right.png");
+disparityRaw = imread("Run 2\depth\image000000_disp_raw.png"); 
+frameLeftRect = imread("Run 2\images\image000000_left.png");
+%frameRightRect = imread("Run 2\images\image00000#_right.png");
+
+
+%Create the reprojection matrix from the individual projection matrices
+%https://stackoverflow.com/questions/30607715/reprojection-matrix-q-from-projection-matrices-p-left-and-p-right
+ProjLeft = [872.76 0 766.88 0; 0 872.76 574.20 0; 0 0 1 0];
+ProjRight = [872.76 0 766.88 -43.647; 0 872.76 574.20 0; 0 0 1 0];
 %reprojectionMatrix = [1 0 0 -cx; 0 1 0 -cy; 0 0 0 f; 0 0 1/b 0];
+ReProj = [1 0 0 -ProjLeft(1,3);0 1 0 -ProjLeft(2,3); 0 0 0 ProjLeft(1,1); 0 0 -ProjRight(1,1)/ProjRight(1,4) (ProjLeft(1,3)-ProjRight(1,3))*ProjRight(1,1)/ProjRight(1,4)];
 
-% Read the disparity map in and adjust the invalid values to zero
-
+%Read the disparity map in and adjust the invalid values to zero
 disparityNan = disparityRaw;
 disparityNan(disparityNan==65520) = NaN;
 disparityDouble = im2double(disparityNan);
@@ -21,11 +27,10 @@ subplot(1,3,3)
 imshow(disparityDouble)
 
 %% Compute the point cloud for the system
-%xyzPoints = reconstructScene(disparityDouble,reprojectionMatrix);
-%ptCloud = pointCloud(xyzPoints./1000, 'Color', frameLeftRect)
-
+xyzPoints = reconstructScene(disparityDouble,ReProj);
+ptCloud = pointCloud(xyzPoints./1000, 'Color', frameLeftRect);
 
 %% Display the point cloud
-%player3D = pcplayer([-3, 3], [-3, 3], [0, 8], 'VerticalAxis', 'y', ...
-%    'VerticalAxisDir', 'down');
-%view(player3D, ptCloud);
+player3D = pcplayer([-3, 3], [-3, 3], [0, 8], 'VerticalAxis', 'y', ...
+    'VerticalAxisDir', 'down');
+view(player3D, ptCloud);
