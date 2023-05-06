8e74f56022c2f270a491f5aa33255336198e1bf3
John <jdin0015@student.monash.edu>
2023-05-06 21:10:30 +1000

    Removed code that saves individual point clouds

--- computePointCloud.m
+++ computePointCloud.m
@@ -9,27 +9,29 @@ worldPoints = generateCircleGridPoints(patternDims, centreDistance);
 
 % Detect the calibration pattern from the calibration image set
 calibImageDir = "Calibration Set";
-calibLeftImages = imageDatastore(fullfile(calibImageDir,"\left\"));
-calibRightImages = imageDatastore(fullfile(calibImageDir,"\right\"));
+calibLeftImages = imageDatastore(fullfile(calibImageDir,"/left/"));
+calibRightImages = imageDatastore(fullfile(calibImageDir,"/right/"));
 [imagePoints, imagesUsed] = detectCircleGridPoints(calibLeftImages.Files, calibRightImages.Files, patternDims);
 stereoCamPara = estimateCameraParameters(imagePoints, worldPoints, ImageSize=[768,1024]);
 
 %% Rectify the original dataset and save a copy of it then make a point cloud
 %Read the images
-sampleImageDir = "Run 4\";
-sampleLeftImages = imageDatastore(fullfile(sampleImageDir,"raw\left\"));
-sampleRightImages = imageDatastore(fullfile(sampleImageDir,"raw\right\"));
+sampleImageDir = "Run 4";
+sampleLeftImages = imageDatastore(fullfile(sampleImageDir,"/raw/left/"));
+sampleRightImages = imageDatastore(fullfile(sampleImageDir,"/raw/right/"));
+ptCloudGrouped = cell(1,size(sampleLeftImages.Files,1));
 
 for image_num = 1:size(sampleLeftImages.Files,1)
     [J1,J2,ReProj] = rectifyStereoImages(imread(sampleLeftImages.Files{image_num}),imread(sampleRightImages.Files{image_num}), stereoCamPara);
-    imwrite(J1, fullfile(sampleImageDir,"rectified\left\", sampleLeftImages.Files{image_num}(end-20:end)));
-    imwrite(J2, fullfile(sampleImageDir,"rectified\right\", sampleRightImages.Files{image_num}(end-21:end)));
+    imwrite(J1, fullfile(sampleImageDir,"/rectified/left/", sampleLeftImages.Files{image_num}(end-20:end)));
+    imwrite(J2, fullfile(sampleImageDir,"/rectified/right/", sampleRightImages.Files{image_num}(end-21:end)));
     disparityMap = disparitySGM(rgb2gray(J1),rgb2gray(J2));
     xyzPoints = reconstructScene(disparityMap,ReProj);
     ptCloudNoise = pointCloud(xyzPoints./1000,"Color",J1);
     ptCloudRed = pcdenoise(ptCloudNoise, NumNeighbors=10);
     ptCloudRed = select(ptCloudRed, findPointsInROI(ptCloudRed, [-0.5 0.5 -0.1 0.5 -0.5 0.5]));
-    pcwrite(ptCloudRed, fullfile(sampleImageDir,"pcloud\", strcat(sampleRightImages.Files{image_num}(end-21:end-4),".ply")))
+    ptCloudGrouped{image_num} = ptCloudRed;
+    %pcwrite(ptCloudRed, fullfile(sampleImageDir,"/pcloud", strcat(sampleRightImages.Files{image_num}(end-21:end-4),".ply")))
 end
 
 %% Join the point clouds together
@@ -38,8 +40,10 @@ gridSize = 0.01;
 mergeSize = 0.0005;
 
 %Read the initial point cloud
-ptCloudRef = pcread(fullfile(sampleImageDir,"pcloud\", strcat(sampleRightImages.Files{1}(end-21:end-4),".ply")));
-ptCloudCurrent = pcread(fullfile(sampleImageDir,"pcloud\", strcat(sampleRightImages.Files{2}(end-21:end-4),".ply")));
+%ptCloudRef = pcread(fullfile(sampleImageDir,"/pcloud", strcat(sampleRightImages.Files{1}(end-21:end-4),".ply")));
+%ptCloudCurrent = pcread(fullfile(sampleImageDir,"/pcloud", strcat(sampleRightImages.Files{2}(end-21:end-4),".ply")));
+ptCloudRef = ptCloudGrouped{1};
+ptCloudCurrent = ptCloudGrouped{2};
 
 %Downsample the points to improve accuracy and speed
 fixed = pcdownsample(ptCloudRef, 'gridAverage',gridSize);
@@ -54,7 +58,8 @@ ptCloudScene = pcmerge(ptCloudRef,ptCloudAligned,mergeSize);
 
 %Loop over subsequent clouds, moving our reference cloud along
 for cloud_num = 3:size(sampleLeftImages.Files,1)
-    ptCloudCurrent = pcread(fullfile(sampleImageDir,"pcloud\", strcat(sampleRightImages.Files{cloud_num}(end-21:end-4),".ply")));
+    %ptCloudCurrent = pcread(fullfile(sampleImageDir,"pcloud/", strcat(sampleRightImages.Files{cloud_num}(end-21:end-4),".ply")));
+    ptCloudCurrent = ptCloudGrouped{cloud_num};
     fixed = moving;
     moving = pcdownsample(ptCloudCurrent, 'gridAverage',gridSize);
 
@@ -64,6 +69,8 @@ for cloud_num = 3:size(sampleLeftImages.Files,1)
     ptCloudAligned = pctransform(ptCloudCurrent, accumTform);
     ptCloudScene = pcmerge(ptCloudScene, ptCloudAligned, mergeSize);
 end
+%Save the combined point cloud
+pcwrite(ptCloudScene, fullfile(sampleImageDir,"/pcloud", strcat(sampleImageDir,".ply")))
 
 hAxes = pcshow(ptCloudScene,'VerticalAxis','Y','VerticalAxisDir','Down');
 title('Updated world scene')
